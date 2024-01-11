3a3432e8d03b3178d10aab9169f2729b5a9b030b
iruizz <98923057+iruizz@users.noreply.github.com>
2024-01-11 14:24:08 -0600

    GEN-263 Cleaned up inital Web Repo, moved files up to web directory, deleted baja-app folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          This is the home page
+          
         </p>
         <div>
           <a
