308ecf9fe3138c90a1cfe79c1569978132b02115
iruizz <98923057+iruizz@users.noreply.github.com>
2024-03-24 16:06:38 -0500

    slight updates

--- src/app/utils/aws-config.ts
+++ src/app/utils/aws-config.ts
@@ -4,8 +4,8 @@ const REGION = "us-east-2";
 const dynamoDBClient = new DynamoDBClient({
   region: REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
 });
 
